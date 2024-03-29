394493370ed793536f0ff3198b411b05ff139ce0
Vlad Anisov <anisovlad@outlook.com>
2024-03-29 02:09:28 -0700

    forgot lol

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,4 +2,12 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	opts = {},
+	config = function()
+		require("ibl").setup({
+			scope = {
+				enabled = true,
+				show_exact_scope = true,
+			},
+		})
+	end,
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,36 +1,42 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
-  config = function()
-    local noice = require("noice")
-    local notify = require("notify")
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+    "echasnovski/mini.notify"
+	},
+	config = function()
+		local noice = require("noice")
+		require("mini.notify").setup()
 
-    noice.setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true,     -- use a classic bottom cmdline for search
-        command_palette = true,   -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,   -- add a border to hover docs and signature help
-      },
-    })
+		noice.setup({
+			-- Hide written messages
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+						find = "written",
+					},
+					opts = { skip = true },
+				},
+			},
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		})
 
-    notify.setup({
-      background_colour = "#1a1c23",
-    })
-  end,
+	end,
 }
