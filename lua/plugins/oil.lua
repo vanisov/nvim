eb7f78ca352eb7b82405e2f14835dd57abb7ecfc
Vlad Anisov <anisovlad@outlook.com>
2024-03-28 18:04:30 -0700

    changed some settings for oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,15 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
       default_file_explorer = false,
+      view_options = {
+        show_hidden = true,
+      },
     })
 
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  end,
 }
