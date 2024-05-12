185377c29f5bb0f732a8cf9e77e39950eba4d12b
Vlad Anisov <anisovlad@outlook.com>
2024-05-12 15:09:59 -0700

    fix: removed node_modules and .git files from telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,20 @@ return {
 		tag = "0.1.6",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local telescope = require("telescope")
 			local builtin = require("telescope.builtin")
 			local keymap = vim.keymap.set
+
+			telescope.setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules", ".git" },
+				},
+			})
+
 			-- File Pickers
 			keymap("n", "<c-p>", builtin.find_files, {})
 			keymap("n", "<leader>ff", builtin.find_files, {})
-      keymap("n", "<leader>gf", builtin.git_files, {})
+			keymap("n", "<leader>gf", builtin.git_files, {})
 			keymap("n", "<leader>fg", builtin.live_grep, {})
 
 			-- Vim Pickers
