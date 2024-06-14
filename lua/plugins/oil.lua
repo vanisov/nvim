32c4faaa5feae47a033d3a1af3a616ba32352a11
Vlad Anisov <anisovlad@outlook.com>
2024-06-14 00:25:35 -0700

    chore: added some new stuff to oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,27 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      default_file_explorer = true,
-      view_options = {
-        show_hidden = true,
-      },
-      float = {
-        padding = 5,
-      }
-    })
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
 
-    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-    vim.keymap.set("n", "<C-n>", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-  end,
+		oil.setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				natural_order = true,
+			},
+			float = {
+				padding = 5,
+			},
+			win_options = {
+				wrap = true,
+			},
+		})
+
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<C-n>", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	end,
 }
